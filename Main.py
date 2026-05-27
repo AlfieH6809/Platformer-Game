2cc10dd33b8892f03cb1e6de840a7d408defe6f3
AlfieH6809 <2425244@brunel.ac.uk>
2026-05-27 20:39:32 +0100

    PYG 1.6

--- Main.py
+++ Main.py
@@ -1,2 +1,14 @@
-# Pygame Init
+# Pygame Init + Import
+
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((1080, 960))
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #user closes Pygame
+            sys.exit()
+
 

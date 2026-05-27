80f70773dccebd4c129f012fd2088b98c09b9a28
AlfieH6809 <2425244@brunel.ac.uk>
2026-05-27 20:55:03 +0100

    PYG 1.8

--- Main.py
+++ Main.py
@@ -2,19 +2,24 @@
 
 import pygame, sys
 
+# Variables for Screen Height + Width
+game_width = 1080 #Screen Width
+game_height = 960 # Screen Height
+
 pygame.init()
-
-game_width = 1080
-game_height = 960
-
 screen = pygame.display.set_mode((game_width, game_height))
 pygame.display.set_caption("Programming Assignment") # Window Title
+clock = pygame.time.Clock() # Frame Rate
 
 while True:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #user closes Pygame
+            pygame.quit()
             sys.exit()
+
+        pygame.display.update()
+        clock.tick(60) #Run the game at 60FPS - Updates the screen at a rate of 60 frames per second
+        
 
 
 

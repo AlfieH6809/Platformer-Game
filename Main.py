24afb02e8d55571374e3a1dfa45951ded8644382
AlfieH6809 <2425244@brunel.ac.uk>
2026-05-27 21:05:31 +0100

    PYG 1.8

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-# Pygame Init + Import
+# pygame Init + Import
 
 import pygame, sys
 
@@ -11,15 +11,19 @@ screen = pygame.display.set_mode((game_width, game_height))
 pygame.display.set_caption("Programming Assignment") # Window Title
 clock = pygame.time.Clock() # Frame Rate
 
+def draw():
+    screen.fill("Blue")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #user closes Pygame
             pygame.quit()
             sys.exit()
 
+        draw()
         pygame.display.update()
         clock.tick(60) #Run the game at 60FPS - Updates the screen at a rate of 60 frames per second
-        
+
 
 
 

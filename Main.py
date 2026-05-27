11ad1d3b00e52673edf759b2f6f677708243c5e0
AlfieH6809 <2425244@brunel.ac.uk>
2026-05-27 20:47:40 +0100

    PYG 1.8

--- Main.py
+++ Main.py
@@ -3,7 +3,11 @@
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((1080, 960))
+
+game_width = 1080
+game_height = 960
+
+screen = pygame.display.set_mode((game_width, game_height))
 pygame.display.set_caption("Programming Assignment") # Window Title
 
 while True:
@@ -13,7 +17,7 @@ while True:
             sys.exit()
 
 
-# Variables (Screen Width + Height saved as variables so that can be referenced)
 
-Game_Width = 1080 #Width of Screen
-Game_Height = 960
+
+
+
